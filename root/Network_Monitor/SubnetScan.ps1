6fd1e28cf551c40f6a6690b9f85186d442c453d8
static7777 <static132@protonmail.com>
2022-10-21 16:12:03 -0600

    Update SubnetScan.ps1

--- root/Network_Monitor/SubnetScan.ps1
+++ root/Network_Monitor/SubnetScan.ps1
@@ -1,40 +1,76 @@
 ### Subnet scan
+Function Build-Menu($title, $subtitle, [Array[]]$options) {
+	$gettingSelection = $True
+	while($gettingSelection) {
+		Clear-Host
+
+		Write-Host -fore yellow "################################"
+		Write-Host -fore cyan "`n $title `n"
+		Write-Host -fore yellow "################################"
+		Write-Host -fore magenta "`n $subtitle `n"
+
+		for ($i=0; $i -lt $options.length; $i++) {
+			if($i -eq $global:pos) {
+				Write-Host -fore green " >  $($options[$i][0])"
+			} else {
+				Write-Host -fore gray "    $($options[$i][0])"
+
+			}
+		}	
+	
+		Write-Host "`n`n(⬆ /⬇ /↪ /q)"
+
+		$key = $Host.UI.RawUI.ReadKey().virtualkeycode
+
+		Switch($key) {
+			13 { $output = $options[$global:pos][1]; $gettingSelection = $False; $global:pos=0}	# Enter
+			38 { if($global:pos -gt 0) { $global:pos -= 1 } }	# Up
+			40 { if($global:pos -lt $options.length-1) { $global:pos += 1} }	# Down 
+			81 { exit }	# Q
+		}
+	}
+	
+	return $output
+
+}
+
+Function Show-Error($Message) {
+	Write-Host -fore red "`n    $Message `n"
+	Write-Host "Press any key to continue..."
+	$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+}
 
 
-$ValidMaskChunks = @(254, 252, 248, 240, 224, 192, 128, 0)
+$ValidMaskChunks = @("254", "252", "248", "240", "224", "192", "128", "0")
 
 
 Function SubnetScan($network, $mask) {
-
-	ping 1.1.1.1 -c 1
-
 	$net = $network.split(".")
 	$mas = $mask.split(".")
 
 	$StartOfNet = $net
 
-
-	$mas
-	for ($i=0; $i -lt 3; $i++) {
-		if( $mas[$i] -ne 255 -and $mas[$i] -match $ValidMaskChunks) {
-			$NumOfHosts = 255-$mas[$i]
+	for ($i=0; $i -lt 4; $i++) {
+		if($ValidMaskChunks.contains($mas[$i])) {
+			echo "found"
+			$NumOfHosts = 255-[int]$mas[$i]
 			for ($j=0; $j -lt 255; $j+=$NumOfHosts) {
+				$j
+				$NumOfHosts
 				if( $net[$i] -ge $j -and $net[$i] -le $j) {
 					$StartOfNet[$i] = $j
-					break
 				}
 			}
 			
+			$max = [int]$StartOfNet[$i]+[int]$NumOfHosts
 
-			for ($k=$StartOfNet[$i]; $k -le $StartOfNet[$i]+$NumOfHosts; $k++) {
-				
-				### Do all addresses (254) in the next octet	
-				#if($i -le 2) {
-				#	for[$l
-				#}
-
-
-			}
+			for ($k=[int]$StartOfNet[$i]; $k -le $max; $k++) {
+				$addr = $StartOfNet
+				$addr[$i] = $k
+				$addr = $addr -join "."
+				$addr
+				ping $addr /n 1 /w 5 | Where -filter {$_ -match "Reply"}
+			} 
 
 			break
 		}
@@ -65,3 +101,4 @@ switch ($sel) {
 	1 { Current }
 	2 { Custom }
 }
+
