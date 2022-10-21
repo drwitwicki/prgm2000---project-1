80f109ca562e5240d0934329e6ed02dc7025a031
Static7777 <static132@protonmail.com>
2022-10-21 10:32:18 -0600

    Changes to be committed: 	modified:   root/Network_Monitor/SubnetScan.ps1

--- root/Network_Monitor/SubnetScan.ps1
+++ root/Network_Monitor/SubnetScan.ps1
@@ -1,13 +1,34 @@
 ### Subnet scan
 
+$ValidMaskChunks = @(254, 252, 248, 240, 224, 192, 128, 0)
+
 Function SubnetScan($network, $mask) {
 
-	$network
+	$net = $network.split(".")
+	$mas = $mask.split(".")
 
-	$net = $network -split "."
-	$mas = $mask -split "."
 
-	Show-Error ""
+	for ($i=0; $i -lt 4; $i++) {
+		if( $mas[$i} -ne 255 -and $mas[$i] -match $ValidMaskChunks) {
+			$NumOfHosts = 255-$mas[$i]
+			for ($j=0; $j -lt 255; $j+=$NumOfHosts) {
+				if( $net[$i] -ge $j -and $net[$i] -le $j) {
+					$StartOfNet = $j
+					break
+				}
+			}
+			
+
+			for ($k=$StartOfNet; $k -le $StartOfNet+$NumOfHosts; $k++) {
+				### Do this address
+				### Do all addresses (254) in the next octet	
+			}
+
+			break
+		} else {
+			Show-Error "Incorrect Mask"
+		}
+	}
 }
 
 Function Current() {
@@ -18,11 +39,6 @@ Function Custom() {
 	[string]$Network = Read-Host "Network (192.168.0.0) "
 	[string]$SubnetMask = Read-Host "Subnet (255.255.255.0) "	
 
-	echo $Network
-	$Network = $Network -split "."
-	Write-Host $Network | gm
-	Write-Host $Network
-
 	SubnetScan $Network $SubnetMask
 }
 
@@ -31,7 +47,7 @@ $opt+=,@("Current Subnet", 1)
 $opt+=,@("Custom Subnet", 2)
 $opt+=,@("Exit", 3)
 
-$sel = Build-Menu "Subnet Scan" "Select Function" $opt
+$sel = Build-Menu "IPv4 Subnet Scan" "Select Function" $opt
 
 switch ($sel) {
 	1 { Current }
