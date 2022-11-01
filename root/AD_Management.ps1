c83bfe180f96735e6f83314b831d432cd22840b5
drwitwicki <drwitwicki@icloud.com>
2022-11-01 10:37:08 -0600

    modified:   root/AD_Management.ps1 	new file:   root/PCInfo.ps1

--- root/AD_Management.ps1
+++ root/AD_Management.ps1
@@ -2,9 +2,19 @@
 ### Eric Caverly & Dave Witwicki
 ### October 19th, 2022
 
+# Hardcoded name for domain controller
 $DCName = "D-DC01"
 
-#
+<#
+.SYNOPSIS
+Create Organizational Unit in desired location
+
+.DESCRIPTION
+Accepts user input for desired OU name and creates the OU in the desired path as specified in the prior selection menu.
+
+.PARAMETER ADOUPath
+DistinguishedName active directory path selected by the user
+#>
 function CreateOU($ADOUPath) {
 	# Get OU to be created
 	Write-Host "Input OU Name`n" -ForegroundColor Green
@@ -24,8 +34,18 @@ function CreateOU($ADOUPath) {
 	}
 }
 
-#
+<#
+.SYNOPSIS
+Delete desired Organizational Unit
+
+.DESCRIPTION
+Deletes the OU selected by the user in the prior selection menu. In order to ensure successful deletion, the script first removes accidental deletion protections, so use wisely.
+
+.PARAMETER ADOUPath
+DistinguishedName active directory path selected by the user
+#>
 function DeleteOU($ADOUPath) {
+	# Don't delete anything if Domain Root is selected, that would be bad
 	if ($ADOUPath -eq (Get-ADDomain).DistinguishedName) {
 		Write-Host "If you want the script that deletes the domain, you'll have to pay for the premium version" -ForegroundColor Red
 	}
@@ -38,6 +58,16 @@ function DeleteOU($ADOUPath) {
 	}
 }
 
+<#
+.SYNOPSIS
+Displays AD User Groups
+
+.DESCRIPTION
+Using the ADOU Path selected by the user in the prior menu, displays all user groups contained within that scope. If the domain root is selected, should show all user groups on the domain.
+
+.PARAMETER ADOUPath
+DistinguishedName active directory path selected by the user
+#>
 function ViewGroups($ADOUPath) {
 	Invoke-Command -ComputerName $DCName -Credential Delta\Administrator -ScriptBlock {
 		param($ADOUPath)
@@ -45,9 +75,19 @@ function ViewGroups($ADOUPath) {
 	} -ArgumentList $ADOUPath
 }
 
+<#
+.SYNOPSIS
+Creates AD User Groups
+
+.DESCRIPTION
+Prompts the user for the name of a new group, then presents a menu to select the group scope and group category. 
+
+.PARAMETER ADOUPath
+DistinguishedName active directory path selected by the user
+#>
 function CreateGroup($ADOUPath) {
 	Write-Host "Input Group Name (create)`n" -ForegroundColor Green
-	$GroupName_ = Read-Host -Prompt ">"
+	$GroupName = Read-Host -Prompt ">"
 
 	$ScopeOpt = @()
 	$ScopeOpt += , @("Domain Local", 1)
@@ -57,9 +97,9 @@ function CreateGroup($ADOUPath) {
 	$ScopeSel = Build-Menu "AD Groups" "select a scope" $ScopeOpt
 
 	switch ($ScopeSel) {
-		1 {$GroupScope_ = 0}
-		2 {$GroupScope_ = 1}
-		3 {$GroupScope_ = 2}
+		1 {$GroupScope = 0}
+		2 {$GroupScope = 1}
+		3 {$GroupScope = 2}
 	}
 
 	$CatOpt = @()
@@ -69,20 +109,23 @@ function CreateGroup($ADOUPath) {
 	$CatSel = Build-Menu "AD Groups" "select a category" $CatOpt
 
 	switch ($CatSel) {
-		1 {$GroupCategory_ = 0}
-		2 {$GroupCategory_ = 1}
+		1 {$GroupCategory = 0}
+		2 {$GroupCategory = 1}
 	}
 
-	Write-Host $ADOUPath
-	Write-Host $GroupScope_
-	Write-Host $GroupCategory_
-
 	Invoke-Command -ComputerName $DCName -Credential Delta\Administrator -ScriptBlock {
-		param($ADOUPath, $GroupName_, $GroupScope_, $GroupCategory_)
-		New-ADGroup -Name $GroupName_ -GroupCategory $GroupCategory_ -GroupScope $GroupScope_ -Path $ADOUPath -Description "created by script"
-	} -ArgumentList $ADOUPath, $GroupName_, $GroupScope_, $GroupCategory_
+		param($ADOUPath, $GroupName, $GroupScope, $GroupCategory)
+		New-ADGroup -Name $GroupName -GroupCategory $GroupCategory -GroupScope $GroupScope -Path $ADOUPath -Description "created by script"
+	} -ArgumentList $ADOUPath, $GroupName, $GroupScope, $GroupCategory
 }
 
+<#
+.SYNOPSIS
+Delete AD Group
+
+.DESCRIPTION
+Prompts user for the identity of an existing active directory group, and then deletes it.
+#>
 function DeleteGroup() {
 	Write-Host "Input Group Name (delete)`n" -ForegroundColor Green
 	$GroupName = Read-Host -Prompt ">"
@@ -93,6 +136,16 @@ function DeleteGroup() {
 	} -ArgumentList $GroupName
 }
 
+<#
+.SYNOPSIS
+Create AD User
+
+.DESCRIPTION
+Prompts user for the identity of a new user account, then creates it within the AD Path specified in the prior menu.
+
+.PARAMETER ADOUPath
+DistinguishedName active directory path selected by the user
+#>
 function CreateUser($ADOUPath) {
 	Write-Host "Input User Name (create)`n" -ForegroundColor Green
 	$UserName = Read-Host -Prompt ">"
@@ -103,6 +156,13 @@ function CreateUser($ADOUPath) {
 	} -ArgumentList $ADOUPath, $UserName
 }
 
+<#
+.SYNOPSIS
+Delete AD User
+
+.DESCRIPTION
+Promps for the name of a user contained within active directory, then deletes it.
+#>
 function DeleteUser() {
 	Write-Host "Input User Name (delete)`n" -ForegroundColor Green
 	$UserName = Read-Host -Prompt ">"
@@ -113,6 +173,13 @@ function DeleteUser() {
 	} -ArgumentList $UserName
 }
 
+<#
+.SYNOPSIS
+Disable AD User
+
+.DESCRIPTION
+Prompts for name of a user contained within active directory, then disables that account
+#>
 function DisableUser() {
 	Write-Host "Input User Name (disable)`n" -ForegroundColor Green
 	$UserName = Read-Host -Prompt ">"
@@ -123,6 +190,13 @@ function DisableUser() {
 	} -ArgumentList $UserName
 }
 
+<#
+.SYNOPSIS
+View and unlock locked user accounts on active directory
+
+.DESCRIPTION
+Searches for all locked out accounts on the domain, and presents a prompt offering to unlock each one.
+#>
 function UnlockUsers() {
 	Write-Host "Account Unlocker`n`n" -ForegroundColor Green
 	Invoke-Command -ComputerName $DCName -Credential Delta\Administrator -ScriptBlock {
@@ -130,6 +204,13 @@ function UnlockUsers() {
 	}
 }
 
+<#
+.SYNOPSIS
+View most recent login time for a specfied user
+
+.DESCRIPTION
+Prompts for name of a user, then displays the most recent login time for that user
+#>
 function ViewLogon() {
 	Write-Host "Input User Name (logon time)`n" -ForegroundColor Green
 	$UserName = Read-Host -Prompt ">"
@@ -140,6 +221,13 @@ function ViewLogon() {
 	} -ArgumentList $UserName
 }
 
+<#
+.SYNOPSIS
+Views all unsuccessful logins on the domain
+
+.DESCRIPTION
+Searches all users on AD to find those with more than one failed login attempt, then displays pertinent information about those users, including the number of failed attempts and bad password entries.
+#>
 function ViewFailedLogon() {
 	Write-Host "Unsuccessful Logins`n`n" -ForegroundColor Green
 	Invoke-Command -ComputerName $DCName -Credential Delta\Administrator -ScriptBlock {
@@ -147,33 +235,45 @@ function ViewFailedLogon() {
 	}
 }
 
+# run forever
 $running = $true
 
 while ($running) {
+	# create array to contain user-readable OU names
 	$CanonicalNamesArray = @()
+	# first entry should be root name of the domain
 	$CanonicalNamesArray += (Get-ADDomain).Forest
+	# get a list of all the user-readable OU names
 	$CanonicalNamesList = Get-ADOrganizationalUnit -Properties CanonicalName -Filter * | Select-Object -ExpandProperty CanonicalName
 
+	# add the list of names to the proper array
 	foreach($name in $CanonicalNamesList) {
 		$CanonicalNamesArray += $name
 	}
 
+	# create an array to hold the distingushed names we need for our scripts to function properly
 	$DistinguishedNamesArray = @()
+	# first entry should be the domain root
 	$DistinguishedNamesArray += (Get-ADDomain).DistinguishedName
+	# get a list of all the distinguished path names
 	$DistinguishedNamesList = Get-ADOrganizationalUnit -Properties DistinguishedName -Filter * | Select-Object -ExpandProperty DistinguishedName
 
+	# add the list of distinguished name paths to the proper array
 	foreach($name in $DistinguishedNamesList) {
 		$DistinguishedNamesArray += $name
 	}
 
+	# takes the user-friendly canonical names and puts them in a new array that our menu function can display properly
 	$opt = @()
 	for ($i = 0; $i -lt $CanonicalNamesArray.length; $i++) {
 		$item = $CanonicalNamesArray[$i]
 		$opt += , @("$item", "$i")
 	}
 
+	# call the menu that displays our entire active directory
 	$sel = Build-Menu "Active Directory" "select OU to modify" $opt
 
+	# build main function selection menu
 	$opt2 = @()
 	$opt2 += , @("Create OU", 1)
 	$opt2 += , @("Delete OU", 2)
@@ -187,8 +287,10 @@ while ($running) {
 	$opt2 += , @("View User Logon Time", 10)
 	$opt2 += , @("View Failed Logins", 11)
 
+	# call the menu that the user interacts with to select a program function
 	$sel2 = Build-Menu "Active Directory" "select function" $opt2
 
+	# choose a function depending on what selection the user made
 	switch ($sel2) {
 		1 {CreateOU($DistinguishedNamesArray[$sel])}
 		2 {DeleteOU($DistinguishedNamesArray[$sel])}
@@ -203,6 +305,7 @@ while ($running) {
 		11 {ViewFailedLogon}
 	}
    
+	# show something so the display doesn't immediately clear
 	Show-Message "Completed" Blue
 
 }
---
+++ root/PCInfo.ps1
@@ -0,0 +1,57 @@
+### Create/Delete OUs
+### Eric Caverly & Dave Witwicki
+### October 19th, 2022
+
+# Get desired computer name from user
+Write-Host "Input Computer Name`n" -ForegroundColor Green
+$ComputerName = Read-Host -Prompt ">"
+
+# Test to see if the computer exists
+$RemoteConnection = Test-Connection $ComputerName -Count 1 -Quiet
+
+if ($RemoteConnection -eq "True") {
+    # Get desired hardware information
+    $ComputerHW = Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $ComputerName | Select-Object Manufacturer, Model | Format-Table -AutoSize
+    $ComputerCPU = Get-CimInstance -ClassName Win32_Processor -ComputerName $ComputerName | Select-Object DeviceID, Name | Format-Table -AutoSize
+    $ComputerMemTotal = Get-CimInstance -ClassName Win32_PhysicalMemoryArray -ComputerName $ComputerName | Select-Object MemoryDevices, MaxCapacity | Format-Table -AutoSize
+    $ComputerMem = Get-CimInstance -ClassName Win32_PhysicalMemory -ComputerName $ComputerName | Select-Object DeviceLocator, Manufacturer, PartNumber, Capacity, Speed | Format-Table -AutoSize
+    $ComputerDisks = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" -ComputerName $ComputerName | Select-Object DeviceID, VolumeName, Size, FreeSpace | Format-Table -AutoSize
+    $ComputerOS = Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $ComputerName | Select-Object Version
+
+    # Figure out what OS the computer is running from the version string
+    switch -Wildcard ($ComputerOS) {
+        "6.1.7600" {$OS = "Windows 7"; break}
+        "6.1.7601" {$OS = "Windows 7 SP1"; break}
+        "6.2.9200" {$OS = "Windows 8"; break}
+        "6.3.9600" {$OS = "Windows 8.1"; break}
+        "10.0.10240" {$OS = "Windows 10 1507"; break}
+        "10.0.10586" {$OS = "Windows 10 1511"; break}
+        "10.0.14393" {$OS = "Windows 10 1607"; break}
+        "10.0.15063" {$OS = "Windows 10 1703"; break}
+        "10.0.16299" {$OS = "Windows 10 1709"; break}
+        "10.0.17134" {$OS = "Windows 10 1803"; break}
+        "10.0.17763" {$OS = "Windows 10 1809"; break}
+        "10.0.18362" {$OS = "Windows 10 1903"; break}
+        "10.0.18363" {$OS = "Windows 10 1909"; break}
+        "10.0.19041" {$OS = "Windows 10 2004"; break}
+        "10.0.19042" {$OS = "Windows 10 20H2"; break}
+        "10.0.19043" {$OS = "Windows 10 21H1"; break}
+        "10.0.19044" {$OS = "Windows 10 21H2"; break}
+        "10.0.22000" {$OS = "Windows 11 21H2"; break}
+        "10.0.22621" {$OS = "Windows 11 22H2"; break}
+        default {$OS = "Unknown Operating System"; break}
+    }
+
+    # Output information
+    Write-Host "Computer Name: $ComputerName"
+    Write-Host "Operating System: $OS"
+    Write-Output $ComputerHW
+    Write-Output $ComputerCPU
+    Write-Output $ComputerMemTotal
+    Write-Output $ComputerMem
+    Write-Output $ComputerDisks
+}
+# If the computer cannot be reached
+else {
+    Write-Host "Computer Unreachable or Nonexistent" -ForegroundColor Red
+}
